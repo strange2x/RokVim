a85f1aac881285d179d8677b1c3c94c6d9f817a7
strange2x <abeeskhan@gmail.com>
2021-08-30 20:26:50 +0530

    Added localvars in default keymaps

--- lua/Conf/DefaultKeymaps.lua
+++ lua/Conf/DefaultKeymaps.lua
@@ -1,20 +1,14 @@
-local map = vim.api.nvim_set_keymap
-local options = {silent = true, noremap = true}
-local VIM_MODES = {
-  Normal = "n",
-  Visual = "v",
-  Insert = "i"
-}
+local CommonVars = require("CommonVars")
 
 -- Remapping window navigation
-map(VIM_MODES.Normal, "<C-l>", "<cmd>wincmd l<CR>", options)
-map(VIM_MODES.Normal, "<C-h>", "<cmd>wincmd h<CR>", options)
-map(VIM_MODES.Normal, "<C-j>", "<cmd>wincmd j<CR>", options)
-map(VIM_MODES.Normal, "<C-k>", "<cmd>wincmd k<CR>", options)
+CommonVars.map(CommonVars.VIM_MODES.Normal, "<C-l>", "<cmd>wincmd l<CR>", CommonVars.options)
+CommonVars.map(CommonVars.VIM_MODES.Normal, "<C-h>", "<cmd>wincmd h<CR>", CommonVars.options)
+CommonVars.map(CommonVars.VIM_MODES.Normal, "<C-j>", "<cmd>wincmd j<CR>", CommonVars.options)
+CommonVars.map(CommonVars.VIM_MODES.Normal, "<C-k>", "<cmd>wincmd k<CR>", CommonVars.options)
 
 -- Moving between buffers
-map(VIM_MODES.Normal, "<leader>.", "<cmd>bn<CR>", options)
-map(VIM_MODES.Normal, "<leader>,", "<cmd>bp<CR>", options)
+CommonVars.map(CommonVars.VIM_MODES.Normal, "<leader>.", "<cmd>bn<CR>", CommonVars.options)
+CommonVars.map(CommonVars.VIM_MODES.Normal, "<leader>,", "<cmd>bp<CR>", CommonVars.options)
 
 -- Closing a buffer
-map(VIM_MODES.Normal, "<leader>q", "<cmd>bd<CR>", options)
+CommonVars.map(CommonVars.VIM_MODES.Normal, "<leader>q", "<cmd>bd<CR>", CommonVars.options)
